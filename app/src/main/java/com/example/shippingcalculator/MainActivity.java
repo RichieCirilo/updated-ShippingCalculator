d24cfc5dba7ec298221a80514dc900c1ca6ac1d9
RichieCirilo <56743016+RichieCirilo@users.noreply.github.com>
2020-09-01 20:27:50 -0700

    updated

--- app/src/main/java/com/example/shippingcalculator/MainActivity.java
+++ app/src/main/java/com/example/shippingcalculator/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.shippingcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText weightET;
+    ShipItem shipItem;
+    TextView baseTV, addedTV, totalTV;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weightET = (EditText)findViewById(R.id.editTextWeight);
+        shipItem = new ShipItem();
+        baseTV = (TextView)findViewById(R.id.baseCostNum);
+        addedTV = (TextView)findViewById(R.id.addCostNum);
+        totalTV = (TextView)findViewById(R.id.totalCostNum);
 
+        weightET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    shipItem.setWeight(Double.parseDouble(s.toString()));
+                }
+                catch(NumberFormatException e){
+                    shipItem.setWeight(0.0);
+                }
+                displayShipping();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
-
-
-
+    public void displayShipping()
+    {
+        baseTV.setText(shipItem.getBaseCost().toString());
+        addedTV.setText(shipItem.getAddedCost().toString());
+        totalTV.setText(shipItem.getTotalCost().toString());
+    }
 
 }
---
+++ app/src/main/java/com/example/shippingcalculator/ShipItem.java
@@ -0,0 +1,46 @@
+package com.example.shippingcalculator;
+public class ShipItem {
+    Double weight;
+    static final Double BASE = 3.00;
+    static final Double ADDED = 0.50;
+    static final Double EXTRA = 4.00;
+    static final int BASE_WEIGHT = 16;
+    static final int EXTRA_WEIGHT = 30;
+
+    public ShipItem()
+    {
+        weight = 0.0;
+    }
+
+    public ShipItem(Double weight)
+    {
+        this.weight = weight;
+    }
+
+    public Double getBaseCost()
+    {
+        if(weight > EXTRA_WEIGHT)
+        {
+            return EXTRA;
+        }
+        return BASE;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getAddedCost()
+    {
+        if(weight > BASE_WEIGHT)
+        {
+            Double overage = (weight - BASE_WEIGHT);
+            return Math.ceil(overage / 4  * ADDED);
+        }
+        return 0.0;
+    }
+    public Double getTotalCost()
+    {
+        return getBaseCost() + getAddedCost();
+    }
+}
